b4fc174fdbf6b6450b9367c4d6818ccba6b98c1c
Michael Dawson-Haggerty <mikedh@kerfed.com>
2020-12-27 21:02:54 -0500

    fix path

--- ci/embree3.bash
+++ ci/embree3.bash
@@ -2,7 +2,7 @@
 set -xe
 
 # Fetch the archive from GitHub releases.
-wget https://github.com/embree/embree/releases/download/v3.12.1/embree-3.12.1.x86_64.linux.tar.gz
+wget https://github.com/embree/embree/releases/download/v3.12.1/embree-3.12.1.x86_64.linux.tar.gz -O /tmp/embree.tar.gz
 
 # check the sha hash
 echo "5e218dd4e95c035c04aa893f7b1169ade11ecc57580e0027eae2ec84cdc9baff  /tmp/embree.tar.gz" | sha256sum --check
