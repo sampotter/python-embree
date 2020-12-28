b4aab174e70b82efc31db1643a25ba82f563e015
Michael Dawson-Haggerty <mikedh@kerfed.com>
2020-12-27 21:27:24 -0500

    no ipsc

--- ci/embree3.bash
+++ ci/embree3.bash
@@ -7,12 +7,12 @@ wget -nv https://github.com/embree/embree/archive/v3.12.1.zip -O /tmp/embree.zip
 # check the sha hash
 echo "ef4c6e4e1386e2e93fc46fc4546bf7a5f7c7f9dcd4a64b0041acdd11f4d1e830  /tmp/embree.zip" | sha256sum --check
 cd /tmp
-unzip embree.zip
+unzip -q embree.zip
 cd embree-3.12.1
 
 mkdir build
 cd build
-ccmake ..
+cmake .. -D EMBREE_ISPC_SUPPORT=0
 
 make
 make install
