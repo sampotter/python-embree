636e8a8f7c5f89c495ce947de0d4337b36e948fb
Michael Dawson-Haggerty <mikedh@kerfed.com>
2020-12-27 20:55:26 -0500

    see how they build

---
+++ ci/embree3.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -xe
+
+# Fetch the archive from GitHub releases.
+wget https://github.com/embree/embree/releases/download/v3.12.1/embree-3.12.1.x86_64.linux.tar.gz
+
+# check the sha hash
+echo "5e218dd4e95c035c04aa893f7b1169ade11ecc57580e0027eae2ec84cdc9baff  /tmp/embree.tar.gz" | sha256sum --check
+# extract
+tar -xzf /tmp/embree.tar.gz --strip-components=1 -C /usr/local
+# remove archive
+rm -rf /tmp/embree.tar.gz
+
---
+++ tests/test_basic.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class BasicTest(unittest.TestCase):
+
+    def test_import(self):
+
+        import embree
+
+
+if __name__ == '__main__':
+    unittest.main()
