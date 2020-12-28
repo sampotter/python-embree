b105450d6e7b8dc97746cc1d13d2140083100424
Michael Dawson-Haggerty <mikedh@kerfed.com>
2020-12-27 21:15:26 -0500

    build from source in ci

--- ci/embree3.bash
+++ ci/embree3.bash
@@ -2,12 +2,17 @@
 set -xe
 
 # Fetch the archive from GitHub releases.
-wget https://github.com/embree/embree/releases/download/v3.12.1/embree-3.12.1.x86_64.linux.tar.gz -O /tmp/embree.tar.gz
+wget https://github.com/embree/embree/archive/v3.12.1.zip -O /tmp/embree.zip
 
 # check the sha hash
-echo "5e218dd4e95c035c04aa893f7b1169ade11ecc57580e0027eae2ec84cdc9baff  /tmp/embree.tar.gz" | sha256sum --check
-# extract
-tar -xzf /tmp/embree.tar.gz --strip-components=1 -C /usr/local
-# remove archive
-rm -rf /tmp/embree.tar.gz
+echo "ef4c6e4e1386e2e93fc46fc4546bf7a5f7c7f9dcd4a64b0041acdd11f4d1e830  /tmp/embree.tar.gz" | sha256sum --check
+cd /tmp
+unzip embree.zip
+cd embree-3.12.1
 
+mkdir build
+cd build
+ccmake ..
+
+make
+make install
