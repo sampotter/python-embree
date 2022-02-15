7295e491c82b9bd6ab52014146b015925d8e8376
Michael Dawson-Haggerty <mikedh@kerfed.com>
2022-02-15 09:58:11 -0500

    try include

--- ci/embree3.bash
+++ ci/embree3.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -xe
 
+pip install tbb-devel
+
+PIPLOC="$(dirname `which pip`)"
+export TBB_DIR=$PIPLOC/../lib
+export TBB_INCLUDE_DIR=$PIPLOC/../include/tbb
+
 # Fetch the archive from GitHub releases.
 wget -nv https://github.com/embree/embree/archive/v3.13.2.zip -O /tmp/embree.zip
 
