07abc811385efa28c72865c4eee8cc68a8b6a98c
Michael Dawson-Haggerty <mikedh@kerfed.com>
2022-02-15 11:31:40 -0500

    silly cache

--- ci/embree3.bash
+++ ci/embree3.bash
@@ -3,9 +3,14 @@ set -xe
 
 VERSION="3.13.3"
 
-wget -nv https://github.com/embree/embree/releases/download/v${VERSION}/embree-${VERSION}.x86_64.linux.tar.gz -O embree.tar.gz
+rm -rf /tmp/embree.tar.gz
+rm -rf ~/embree
+
+wget -nv https://github.com/embree/embree/releases/download/v${VERSION}/embree-${VERSION}.x86_64.linux.tar.gz -O /tmp/embree.tar.gz
+cd /tmp
 tar -zxvf embree.tar.gz
 rm -f embree.tar.gz
+
 mv embree-${VERSION}.x86_64.linux ~/embree
 
 
