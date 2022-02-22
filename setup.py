e47f11a58c9f8c9482e5e3714507cae1bd244e8d
Michael Dawson-Haggerty <mikedh@kerfed.com>
2022-02-22 14:00:10 -0500

    add version

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ import os
 
 from setuptools import find_packages, setup
 from setuptools.extension import Extension
-
 from Cython.Build import cythonize
 
 extensions = [
     Extension(
         'embree',
-        ['embree.pyx'],
+        ['embree/embree.pyx'],
         libraries=['embree3'],
         include_dirs=[
             '/opt/local/include',
@@ -19,8 +18,22 @@ extensions = [
     )
 ]
 
+cwd = os.path.abspath(os.path.expanduser(
+    os.path.dirname(__file__)))
+with open(os.path.join(cwd, 'README.md'), 'r') as f:
+    long_description = f.read()
+with open(os.path.join(cwd, 'embree', 'version.py'), 'r') as f:
+    # use eval to get a clean string of version from file
+    __version__ = eval(f.read().strip().split('=')[-1])
+
+
 setup(
     name='embree',
+    version=__version__,
+    description='Ray queries on triangular meshes.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['numpy'],
     packages=find_packages(),
     ext_modules=cythonize(extensions),
     zip_safe=False
